c2fc948ae3bb3f56df009a2433eeaccf5d610c75
Anton Darmenov <anton.s.darmenov@nasa.gov>
2022-06-21 22:42:00 -0400

    Analytical implementation of VIIRS pixel area
    The tabulated VIIRS 375m pixel area data was replaced by
    segmented polynomial regression model. The new implementation
    does not do I/O and is vectorized.

--- src/qfed/fire_products.py
+++ src/qfed/fire_products.py
@@ -471,54 +471,40 @@ def modis_pixel_area(sample):
     return area
 
 
-def viirs_pixel_area(sample, file='pix_size_viirs_iband.txt'):
+def viirs_pixel_area(sample):
     '''
-    Reads a file containing the along scan and along track pixel 
-    dimensions (in km) as a function of sample number or scan angle 
-    for the VIIRS 375m data.
-    Note the corresponding image is 6400 samples wide while the file 
-    gives you 3200 values since pixels grow from nadir to the edge of 
-    the image creating a mirror effect. In this case, sample = 0 at nadir 
-    and sample = 3199 at the edge (either left or right) of the image.
-    '''
+    A. Darmenov - Segmented polynomial regression model 
+    fitted to VIIRS 375m pixel dimensions data provided by 
+    Wilfrid Schroeder (NOAA; personal communication)
+ 
+    Note that:
+        sample = 0     at nadir
+        sample = 3199  at the edge (either left or right)
 
-    data_description = {'names': ('sample', 'along_scan', 'along_track', 'scan_angle'),
-                        'formats': ('i4', 'f4', 'f4', 'f4')}
-
-    data = np.loadtxt(file, dtype=data_description, delimiter=',', skiprows=1)
+    The tabulated data has two points of discontinuity:
+        breakpoint = (1184, 1920)    
+    hence there are three intervals:   
+        interval = ((0, 1184), (1184, 1920), (1920, 3199))
     
-    smpl = data['sample']
-    _area = data['along_scan'] * data['along_track']
+    Pixel area data in each interval was fitted to a polynomial
+    of degree 5. The coefficients of the three polynomials, 
+    in decreasing powers, are included in the code.
+    '''
 
-    # mirror area
-    # note: if nadir is _area[0], then
-    # we could do 
-    #     1) area = np.concatenate((_area[::-1], _area[1:])
-    # or 
-    #     2) area = np.concatenate((_area[:0:-1], _area[:])
-    # however the length of the array would be 6400-1!?
-    #
-    # looking at the scan angle values (they are > 0),
-    # one needs to likely mirror the entire array, i.e.
-    #      3) area = np.concatenate((_area[::-1], _area[:]))
-    # this will construct an array with length 6400
+    # reconstruct the pixel area data
+    _area = np.zeros(3200)
 
-    # this should work in a way similar to (1), 
-    # however np.int16 needs to be hardcoded to
-    # avoid underflow (sample is of type uint16) when sample - 3199 < 0
-    #   sample = np.abs(sample - 3199, dtype=np.int16)
-    #   return _area[sample]
-    # or the equivalent but slower
-    #   area = [_area[np.abs(i - 3199)] for i in sample]
-    #   return area
+    interval = ((0, 1184), (1184, 1920), (1920, 3199))
+    p_fit = (np.poly1d(( 7.50716369,   -2.80021144,   9.60939363e-01,   4.45427672e-01,  4.19643416e-03, 1.38396491e-01)),
+             np.poly1d((31.83780167,  -63.73430877,      53.82525724,     -22.64615702,      4.97063667,    -0.32549506)),
+             np.poly1d((36.99433091, -132.42437447,     191.84304112,    -139.19738588,      50.6919331,    -7.28395961)))
 
-    # equivalent to 3) but possibly more fragile and less readable
-    #      4) area = _area[np.abs(np.array(sample - 3199.5, dtype=np.int16))]
-    # ...go with 3)
-
-    area = np.concatenate((_area[::-1], _area[:]))[sample]
-
-    return area 
+    for ((s_i, s_e), p) in zip(interval, p_fit):
+        _area[s_i:s_e] = p(np.arange(s_i, s_e)/3200.0)
+ 
+    # sample the reconstructed data
+    area = _area[np.abs(sample)]
+    return area
 
 
 def __test__():
