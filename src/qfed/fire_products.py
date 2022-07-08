bfe2049946ba587cf56e57e42ecc578d18b0c999
Anton Darmenov <anton.s.darmenov@nasa.gov>
2022-07-08 11:53:11 -0400

    Edit docstring and improve formatting

--- src/qfed/fire_products.py
+++ src/qfed/fire_products.py
@@ -470,16 +470,17 @@ def modis_pixel_area(sample):
 def viirs_pixel_area(sample):
     '''
     A. Darmenov - Segmented polynomial regression model 
-    fitted to VIIRS 375m pixel dimensions data provided by 
-    Wilfrid Schroeder (NOAA; personal communication)
+    fitted to VIIRS 375m pixel dimensions data. VIIRS 
+    data was provided by Wilfrid Schroeder (NOAA; personal 
+    communication).
  
-    Note that in the tabulated data:
+    Note that in the tabular data:
         sample = 0     at nadir
         sample = 3199  at the edge (either left or right)
     whereas in the VIIRS granules/images the samples are 
     in the interval [0, 6399].
 
-    The tabulated data has two points of discontinuity:
+    The tabular data has two points of discontinuity:
         breakpoint = (1184, 1920)    
     hence there are three intervals:   
         interval = ((0, 1184), (1184, 1920), (1920, 3199))
@@ -493,9 +494,16 @@ def viirs_pixel_area(sample):
     _area = np.zeros(3200)
 
     interval = ((0, 1184), (1184, 1920), (1920, 3199))
-    p_fit = (np.poly1d(( 7.50716369,   -2.80021144,   9.60939363e-01,   4.45427672e-01,  4.19643416e-03, 1.38396491e-01)),
-             np.poly1d((31.83780167,  -63.73430877,      53.82525724,     -22.64615702,      4.97063667,    -0.32549506)),
-             np.poly1d((36.99433091, -132.42437447,     191.84304112,    -139.19738588,      50.6919331,    -7.28395961)))
+
+    p_fit = (np.poly1d((  7.50716369,     -2.80021144,   
+                          9.60939363e-01,  4.45427672e-01, 
+                          4.19643416e-03,  1.38396491e-01)),
+             np.poly1d(( 31.83780167,    -63.73430877,
+                         53.82525724,    -22.64615702, 
+                          4.97063667,     -0.32549506)),
+             np.poly1d(( 36.99433091,   -132.42437447,
+                        191.84304112,   -139.19738588,
+                         50.69193310,     -7.28395961)))
 
     for ((s_i, s_e), p) in zip(interval, p_fit):
         _area[s_i:s_e] = p(np.arange(s_i, s_e)/3200.0)
