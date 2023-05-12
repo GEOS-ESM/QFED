2603e5d2218b3768d5b8ff5cee2b2316b44ec1ff
Anton Darmenov <anton.s.darmenov@nasa.gov>
2023-05-12 11:24:20 -0400

    Add log messages.

--- src/qfed/inventory.py
+++ src/qfed/inventory.py
@@ -41,13 +41,16 @@ class Finder():
         a fire product file, and a vegetation file. The list includes files with
         observations in the time window specified by the two arguments.
         '''
+
+        logging.info(f"Starting search for input files containing data between {t_start} and {t_end}.")
+
         result = []
 
         t = t_start
         while t < t_end:
             search_path = self._fp_file.format(t)
             
-            logging.debug(("Searching for fire product files " 
+            logging.debug((f"Searching for fire product files " 
                            f"matching pattern '{os.path.basename(search_path)}' "
                            f"in directory '{os.path.dirname(search_path)}'."))
 
@@ -57,18 +60,27 @@ class Finder():
 
                 gp_file = self._gp_file.format(t)
 
-                logging.debug(f"Found a match: ({t}, {gp_file}, {fp_file})")
-
                 _item = Item(time=t, geolocation=gp_file, fire=fp_file,
                     vegetation=self._vegetation_file)
                 
+                logging.debug(f"Found a match: {_item}.")
+                
                 result.append(_item)
                 
-                logging.info(f"Added {_item} to queue for processing.")
+                logging.debug(f"Added {_item} to queue for processing.")
             else:
-                logging.debug(f"Did not find files matching {search_path}.")
+                logging.debug(f"Did not find files matching the pattern '{search_path}'.")
 
             t = t + timedelta(seconds=self._time_interval)
+
+        if result:
+            logging.info((f"Search for input files completed. "
+                          f"{len(result)} fire product files were identified "
+                          f"and added to the processing queue.\n"))
+        else:
+            logging.warning((f"Search for input files completed. "
+                             f"However, no fire product files were identified "
+                             f"that meet the search criteria.\n")) 
 
         return result
 
