196c6dd10f8f64534d5822c7aeb698f809f9aeb8
Anton Darmenov <anton.s.darmenov@nasa.gov>
2023-05-12 14:49:51 -0400

    Update tests.

--- tests/qfed/test_fire_products.py
+++ tests/qfed/test_fire_products.py
@@ -3,6 +3,7 @@ import unittest
 import os
 import sys
 
+from qfed.instruments import Instrument, Satellite
 import qfed.fire_products
 
 class test(unittest.TestCase):
@@ -14,21 +15,30 @@ class test(unittest.TestCase):
         modis_dir = '/discover/nobackup/dao_ops/intermediate/flk/modis/006'
         viirs_dir = '/discover/nobackup/projects/eis_fire/data/VIIRS/Level2'
     
-        instance = {'modis/terra' : os.path.join(modis_dir, 'MOD14',    '2020', '300', 'MOD14.A2020300.1215.006.NRT.hdf'),
-                    'modis/aqua'  : os.path.join(modis_dir, 'MYD14',    '2020', '300', 'MYD14.A2020300.1215.006.NRT.hdf'),
-                    'viirs/npp'   : os.path.join(viirs_dir, 'VNP14IMG', '2020', '300', 'VNP14IMG.A2020300.1142.001.2020300194419.nc'),
-                    'viirs/jpss-1': os.path.join(viirs_dir, 'VJ114IMG', '2020', '300', 'VJ114IMG.A2020300.1054.002.2020300170004.nc')}
-    
-        for id, file in instance.items():
-            instrument, satellite = id.split('/')
-            print('instrument: {0:s}'.format(id))
-            print('file: {0:s}'.format(os.path.basename(file)), file)
+        queue = {
+            (Instrument.MODIS, Satellite.TERRA) : os.path.join(modis_dir, 'MOD14',    '2020', '300', 'MOD14.A2020300.0850.006.NRT.hdf'),
+            (Instrument.MODIS, Satellite.AQUA)  : os.path.join(modis_dir, 'MYD14',    '2020', '300', 'MYD14.A2020300.1325.006.NRT.hdf'),
+            (Instrument.VIIRS, Satellite.NPP)   : os.path.join(viirs_dir, 'VNP14IMG', '2020', '300', 'VNP14IMG.A2020300.1142.001.2020300194419.nc'),
+            (Instrument.VIIRS, Satellite.JPSS1) : os.path.join(viirs_dir, 'VJ114IMG', '2020', '300', 'VJ114IMG.A2020300.1054.002.2020300170004.nc')
+            }
+     
+        print(queue)
+
+        for (instrument, satellite), file in queue.items():
+            print(f"{instrument=}")
+            print(f"{satellite=}")
+            print(f"file: {os.path.basename(file)}")
+            print(f"direcory: {os.path.dirname(file)}")
     
             reader = qfed.fire_products.create(instrument, satellite, verbosity=10)
-    
-            print('geolocation file: {0:s}'.format(reader.get_geolocation_file(file)))
-            print('number of fire pixels: {0:d}'.format(reader.get_num_fire_pixels(file)))
-            if reader.get_num_fire_pixels(file) > 0:
+   
+            gp_file = reader.get_geolocation_file(file)
+            print(f"geolocation file: {gp_file}")
+
+            n_fires = reader.get_num_fire_pixels(file)
+            print(f"number of fire pixels: {n_fires}")
+
+            if n_fires > 0:
                 print('fire longitude : ', reader.get_fire_longitude(file))
                 print('fire latitude  : ', reader.get_fire_latitude(file))
                 print('fire frp       : ', reader.get_fire_frp(file))
