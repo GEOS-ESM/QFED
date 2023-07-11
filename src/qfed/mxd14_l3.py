Fix inconsistent calculation of pixel areas
This change addresses an inconsistency in the area of
a pixel calculated from the one-dimensional 'FP_sample'
and the area calculated from the two-dimensional 'sample'
coordinate. IMO, based on the A.F.P. C6 User Guide and
Ichoku and Kaufman (2005), the latter was incorrectly offset
by 1.

src/qfed/mxd14_l3.py
@@ -791,7 +791,7 @@ class MxD14_L3(object):
 #       Calculate pixel area
 #       --------------------
         Area = np.zeros((nLines,nSamples))
-        Area[:] = _pixar(1+np.arange(nSamples))
+        Area[:] = _pixar(np.arange(nSamples))
 
         # non-fire land pixel
         i = np.logical_and(fmask==NOFIRE, valid)