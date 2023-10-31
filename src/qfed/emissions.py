f2e13982e1a210036738ace60f58c8ea9d6e7f85
Anton Darmenov <anton.s.darmenov@nasa.gov>
2023-10-31 08:33:41 -0400

    Calibrate VIIRS/NPP and VIIRS/JPSS-1
    This is the initial calibration of VIIRS/NPP and VIIRS/JPSS-1
    using the 'historical' QFED-v2.5r1 as target emissions.

--- src/qfed/emissions.py
+++ src/qfed/emissions.py
@@ -132,7 +132,7 @@ class Emissions:
             'grassland': 1.0,
         }
 
-        # Scaling of C6 based on C5 (based on OC tuning)
+        # scaling of C6 based on C5 (based on OC tuning)
         alpha_C6 = {
             'MODIS_TERRA': 0.96450253,
             'tropical_forest': 1.09728882,
@@ -151,13 +151,51 @@ class Emissions:
 
         # effective combustion rate
         self._A_f = {}
+
+        self._A_f[(Instrument.MODIS, Satellite.TERRA)] = {}
+        self._A_f[(Instrument.MODIS, Satellite.AQUA)] = {}
         for s in self._EF.keys():
             if s in AEROSOL_SPECIES:
                 enhance_factor = enhance_aerosol_C6
             else:
                 enhance_factor = enhance_gas_C6
 
-            self._A_f[s] = {b: (Alpha * v) for (b, v) in enhance_factor.items()}
+            self._A_f[(Instrument.MODIS, Satellite.TERRA)][s] = {
+                b: (Alpha * v) for (b, v) in enhance_factor.items()
+            }
+
+            self._A_f[(Instrument.MODIS, Satellite.AQUA)][s] = {
+                b: (Alpha * v) for (b, v) in enhance_factor.items()
+            }
+
+
+        alpha_JPSS1 = {
+            'tropical_forest': 1.524*0.40, # 1.524: [1.300, 1.865]
+            'extratropical_forest': 0.640*1.46, # 0.640: [0.485, 0.771]
+            'savanna': 1.224*0.79, # 1.224: [1.075, 1.323]
+            'grassland': 0.830*1.05, # 0.830: [0.770, 0.934]
+        }
+        alpha_NPP = {
+            'tropical_forest': 1.435*0.40, # 1.435: [1.114, 1.800]
+            'extratropical_forest': 0.685*1.46, # 0.685: [0.485, 0.813]
+            'savanna': 1.147*0.79, # 1.147: [1.055, 1.290]
+            'grassland': 0.878*1.05, # 0.878: [0.776, 0.977]
+        }
+        self._A_f[(Instrument.VIIRS, Satellite.JPSS1)] = {}
+        self._A_f[(Instrument.VIIRS, Satellite.NPP)] = {}
+        for s in self._EF.keys():
+            if s in AEROSOL_SPECIES:
+                enhance_factor = enhance_aerosol_C5
+            else:
+                enhance_factor = enhance_gas_C5
+
+            self._A_f[(Instrument.VIIRS, Satellite.JPSS1)][s] = {
+                b: (Alpha * v * alpha_JPSS1[b]) for (b, v) in enhance_factor.items()
+            }
+            self._A_f[(Instrument.VIIRS, Satellite.NPP)][s] = {
+                b: (Alpha * v * alpha_NPP[b]) for (b, v) in enhance_factor.items()
+            }
+
 
         # satellite factors
         self._S_f = {}
@@ -168,8 +206,8 @@ class Emissions:
 
         self._S_f[(Instrument.MODIS, Satellite.TERRA)] = self._S_f['MODIS_TERRA']
         self._S_f[(Instrument.MODIS, Satellite.AQUA)] = self._S_f['MODIS_AQUA']
-        self._S_f[(Instrument.VIIRS, Satellite.JPSS1)] = 1.0  # TODO: TBD
-        self._S_f[(Instrument.VIIRS, Satellite.NPP)] = 1.0    # TODO: TBD
+        self._S_f[(Instrument.VIIRS, Satellite.JPSS1)] = 0.49 #0.384  # TODO: TBD
+        self._S_f[(Instrument.VIIRS, Satellite.NPP)] = 0.54   #0.384    # TODO: TBD
 
     def emission_factor(self, species, fire):
         """
@@ -179,13 +217,13 @@ class Emissions:
         bb = fire.type.name.lower()
         return self._EF[species][bb]
 
-    def effective_combustion_rate(self, species, fire):
+    def effective_combustion_rate(self, platform, species, fire):
         """
         Returns the effective combustion rate parameter
         used in the calculation of emissions.
         """
         bb = fire.type.name.lower()
-        return self._A_f[species][bb]
+        return self._A_f[platform][species][bb]
 
     def satellite_factor(self, platform):
         """
@@ -239,7 +277,7 @@ class Emissions:
                 for bb in self.biomass_burning:
                     # TODO: A_f should be a dict.
                     B_f, eB_f = self.emission_factor(s, bb)
-                    A_f = self.effective_combustion_rate(s, bb)
+                    A_f = self.effective_combustion_rate(p, s, bb)
                     E[s][bb][:, :] += units_factor * A_f * S_f * B_f * FRP[bb]
                     E_[s][bb][:, :] += units_factor * A_f * S_f * B_f * F[bb] * A_
 
@@ -277,22 +315,16 @@ class Emissions:
 
         return result
 
-    def _save_as_netcdf4(self, dir, filename, compress=False, fill_value=1e15):
+    def _save_as_netcdf4(self, file, compress=False, fill_value=1e15):
         """
         Saves gridded emissions to a file. 
         """
-        nymd = 22220101  # 10000*self.date.year + 100*self.date.month + self.date.day
-        nhms = 120000
 
         # map species to output files
         self.file = {
-            species: os.path.join(dir, f'qfed.emis_{species}.{nymd}.nc4'.lower())
+            species: file.format(species=species.lower())
             for species in self.estimate.keys()
         }
-
-        # TODO
-        nymd = 22220101  # 10000*self.date.year + 100*self.date.month + self.date.day
-        nhms = 120000
 
         for species, file in self.file.items():
             # construct meta data for the data variables: name, long name and units
@@ -390,7 +422,7 @@ class Emissions:
 
             f.close()
 
-            logging.info(f"Successfully saved gridded emissions to file '{filename}'.")
+            logging.info(f"Successfully saved gridded emissions to file '{file}'.")
 
     def compress_n4zip(self):
         """
@@ -411,8 +443,7 @@ class Emissions:
 
     def save(
         self,
-        dir,
-        filename,
+        file,
         forecast=None,
         ndays=1,
         compress=False,
@@ -430,7 +461,7 @@ class Emissions:
         # self._save_forecast(forecast, fill_value=1.0e20)
 
         for n in range(ndays):
-            self._save_as_netcdf4(dir, filename, compress in (True,), fill_value)
+            self._save_as_netcdf4(file, compress in (True,), fill_value)
 
             if compress == 'n4zip':
                 self.compress_n4zip()
